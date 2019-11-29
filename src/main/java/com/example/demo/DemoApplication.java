b2d9a27a44a2562b1d7c047574af77c376e4ee65
Wu jinyi <sihuowander@163.com>
2019-11-29 09:04:22 +0800

    1、热部署配置（验证无效） 2、增加默认配置文件的信息读取方式，验证有效

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication 是一个组合注解，等同于
+ * @Configuration
+ * @EnableAutoConfiguration ：
+ * @ComponentScan
+ */
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private String age;
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello Spring Boot!";
+        return name+age;
     }
 }
