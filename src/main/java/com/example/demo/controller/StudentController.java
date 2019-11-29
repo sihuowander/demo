75d09c32026e019294ec813207c2ef50a1962e94
Wu jinyi <sihuowander@163.com>
2019-11-29 15:06:03 +0800

    集成MyBatis功能，验证已成功，注意事项见bz.txt 四

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.mapper.StudentMapper;
+import com.example.demo.pojo.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * Student 控制器
+ *
+ * @author: @我没有三颗心脏
+ * @create: 2018-05-08-下午 20:25
+ */
+@Controller
+public class StudentController {
+
+    @Autowired
+    private StudentMapper studentMapper;
+
+    @RequestMapping("/listStudent")
+    public String listStudent(Model model) {
+        List<Student> students = studentMapper.findAll();
+        model.addAttribute("students", students);
+        return "listStudent";
+    }
+}
