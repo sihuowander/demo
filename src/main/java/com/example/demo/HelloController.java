d1d36be2e6dcab04a67cf7401840c74b4cbf026b
Wu jinyi <sihuowander@163.com>
2019-11-29 09:35:17 +0800

    配置文件封装获取，验证已成功，注意事项见bz.txt

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-//    @Value("${name}")
-//    private String name;
-//    @Value("${age}")
-//    private String age;
-    @Value("${content}")
-    private String content;
+    @Autowired
+    private StudentProperties studentProperties;
+//    @Value("${content}")
+//    private String content;
     @RequestMapping("/hello")
     public String hello(){
-//        return name+age;
-        return content;
+        return studentProperties.getName()+studentProperties.getAge();
+//        return content;
     }
 }
